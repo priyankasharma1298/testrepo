996e947087ba414eb1922290bd06d7c258035eeb
priyankasharma1298 <priyankasharmapce@gmail.com>
2023-01-21 09:29:10 +0530

    string

---
+++ string/string_question.py
@@ -0,0 +1,36 @@
+# string ='zewmyde'    o/p = 5
+# string ='moosmyf'    o/p = 3
+# in one operation you can remove a character from the begining or the end of string
+# the operation can be repeated multiple times
+# FIND MINNIMUM NUMBER OF OPERATIONS NEEDED SO THAT THERE IS A CHARACTER IN THE STRING
+# WHICH HAS A FREQUENCY AT LEAST HALF OF THE LENGTH OF THE STRING 
+
+def min_operations(list_str , len_of_list_str): 
+    j=0 
+    dic_count={}
+    opreation_count=0
+
+    while j < len_of_list_str :
+        list_str.pop() 
+        if len(list_str) == 0:
+            return 0
+        else:    
+            opreation_count+=1
+            half_lenght = len(list_str)//2
+
+            for i in list_str:
+                if i in dic_count:
+                    dic_count[i]+=1
+                else:
+                    dic_count[i]=1
+            
+                for value in dic_count:
+                    if half_lenght == dic_count[value]:
+                        return opreation_count+1 
+
+string ='zewmyde'
+list_str = list(string)
+len_of_list_str = len(list_str)
+print( min_operations( list_str , len_of_list_str ) ) 
+
+        
